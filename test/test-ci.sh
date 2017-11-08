e6aff16232f81678ededa832f3c1c2ef571db4aa
Ryan Sanford <ryansanford@flywheel.io>
2017-11-08 15:04:32 -0600

    Check out osx build

--- test/test-ci.sh
+++ test/test-ci.sh
@@ -3,7 +3,9 @@ set -e
 
 # Set cwd
 unset CDPATH
-cd "$( dirname "${BASH_SOURCE[0]}" )/../docker"
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
+python -c "import distutils.util; print(distutils.util.get_platform())"
 
 
 echo "Tests completed without error."
